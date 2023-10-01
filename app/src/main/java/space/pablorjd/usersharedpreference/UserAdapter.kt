8c4ef864c160818f86bd100dc7a53c15ca48d65c
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-10-01 12:39:44 -0300

    Se cambia el listado desde una lista a unas card views

--- app/src/main/java/space/pablorjd/usersharedpreference/UserAdapter.kt
+++ app/src/main/java/space/pablorjd/usersharedpreference/UserAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import space.pablorjd.usersharedpreference.databinding.ItemUserAltBinding
 import space.pablorjd.usersharedpreference.databinding.ItemUserBinding
 
 class UserAdapter(private val users:List<User>, private val listener: OnClickListener)
@@ -15,7 +16,7 @@ class UserAdapter(private val users:List<User>, private val listener: OnClickLis
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         context = parent.context
 
-        val view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user_alt, parent, false)
 
         return ViewHolder(view)
     }
@@ -43,7 +44,7 @@ class UserAdapter(private val users:List<User>, private val listener: OnClickLis
 
 
     inner class ViewHolder(view:View) :RecyclerView.ViewHolder(view) {
-        val binding = ItemUserBinding.bind(view)
+        val binding = ItemUserAltBinding.bind(view)
 
         fun setListener(user: User, position: Int) {
             binding.root.setOnClickListener {
